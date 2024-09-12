0b3dc559fd8aac6390f3c4f5f351b6ba7eca4ae5
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-13 01:28:43 +0530

    get class name and confidence

--- main.py
+++ main.py
@@ -16,9 +16,31 @@ proto_file = "bvlc_googlenet.prototxt"
 with open('classification_classes_ILSVRC2012.txt', 'r') as f:
     classes = f.read().splitlines()
 
+inHeight = 224
+inWidth = 224
+swap_rgb = False
+mean = [104, 117, 123]
+scale = 1.0
+
+# load model.
+net = cv.dnn.readNet(model=weight_file, config=proto_file, framework= "caffe")
+
+# create a blob object
+blob = cv.dnn.blobFromImage(image, scale, (inWidth, inHeight), mean, swap_rgb, crop=False)
+
+# Run a model
+net.setInput(blob)
+out = net.forward()
+out = out.flatten()
+
+classID = np.argmax(out)
+class_name = classes[classID]
+confidence = f"{round(out[classID] * 100, 3)}%"
+
+print(class_name, confidence)
 
 # Display the image.
 plt.imshow(image[:,:,::-1])
-plt.title("Image of a Panda")
+plt.title(f"Image of a {class_name}, {confidence}")
 plt.axis('off')
 plt.show()
