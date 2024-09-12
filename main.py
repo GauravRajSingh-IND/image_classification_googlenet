6f18d3402d050e1b2bc12cef5eb297a772798df5
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-13 01:20:02 +0530

    read classes and load caffe model

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ path = "panda.jpg"
 # read image.
 image = cv.imread(path, cv.IMREAD_COLOR)
 
+# read model.
+weight_file = "bvlc_googlenet.caffemodel"
+proto_file = "bvlc_googlenet.prototxt"
+
+# read all the classes from txt file and store in a variable..
+with open('classification_classes_ILSVRC2012.txt', 'r') as f:
+    classes = f.read().splitlines()
+
+
 # Display the image.
 plt.imshow(image[:,:,::-1])
 plt.title("Image of a Panda")
